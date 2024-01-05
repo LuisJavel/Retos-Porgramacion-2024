d9f1719164087ec8c83a2a5b085297a62171a251
LMsqz <luisfmasaquiza@hotmail.com>
2024-01-05 13:36:13 +0100

    00 reto programacion python inical

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Javier-msqz.py
@@ -0,0 +1,30 @@
+# https://python.org
+
+# cometario en una liña
+
+"""
+este etambien es un comentario en varias liña
+"""
+
+"""
+estes es otro comentario me auto graba con doble comilla ' ' ' 
+"""
+
+my_variable = "mi variable"
+my_variable = "nueva valor de variable "
+
+MY_CONSTANT = "Mi constante"  # por convención no se cambia
+
+my_entero = 50
+my_flotante = 50.23
+my_bool = True
+my_bool = False
+my_strin = "Mi cade de texto"
+
+print("¡Hola Mundo, Python")
+
+print(type(my_entero))
+print(type(my_flotante))
+print(type(my_bool))
+print(type(my_bool))
+print(type(my_strin))
